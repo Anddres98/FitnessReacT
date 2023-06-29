c6a4c2c917f2a8b790ad3d768a1440d99bc8085f
Andres pardo <fernando.andresp13@gmail.com>
2023-06-29 18:48:40 -0500

    se hizo la page del formulario

--- src/pages/ExerciseNew.js
+++ src/pages/ExerciseNew.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import Card from '../components/Card'
+import ExerciseForm from '../components/ExerciseForm'
 
 class ExercicesNew extends React.Component{
-    
+    /*
+    esto se uso para hacer une vento con el click de un boton
     handleClick = () =>{
         console.log('clicked')
     }
@@ -12,6 +15,108 @@ class ExercicesNew extends React.Component{
             <button onClick={this.handleClick}>
                 Send
             </button>
+        )
+    }
+    */
+    state = {}
+
+    handleSubmit= e =>{
+        e.preventDefault()
+        console.log(this.state)
+    }
+
+    handleChange = e =>{
+        console.log(`${e.target.name}: ${e.target.value}`)
+
+        /*let partialState = {}
+        partialState[e.target.name] = e.target.value
+        this.setState(partialState)*/
+        // esto sustituye al de arriba
+
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+    }
+
+    render(){
+    return(
+
+        <div className="container">
+        <form 
+            onSubmit={this.handleSubmit}
+        >
+            <div className="form-group">
+                <input 
+                    type="text" 
+                    className="form-control" 
+                    placeholder="title" 
+                    name="title"
+
+                    onChange={this.handleChange}
+                    value={this.state.title}
+             
+                />
+            </div>
+            <div className="form-group">
+                <input 
+                    type="text" 
+                    className="form-control" 
+                    placeholder="description" 
+                    name="descriptions"
+
+                    onChange={this.handleChange}
+                    value={this.state.description}
+             
+                />
+            </div>
+            <div className="form-group">
+                <input 
+                    type="text" 
+                    className="form-control" 
+                    placeholder="img" 
+                    name="img"
+
+                    onChange={this.handleChange}
+                    value={this.state.img}
+                   
+                />
+            </div>
+            <div className="form-row">
+                <div className="col">
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        placeholder="leftColor" 
+                        name="leftColor"
+
+                        onChange={this.handleChange}
+                        vale={this.state.leftColor}
+                      
+                    />
+                </div>
+                <div className="col">
+                    <input 
+                        type="text" 
+                        className="form-control"
+                        placeholder="rightColor" 
+                        name="rightColor"
+
+                        onChange={this.handleChange}
+                        value={this.state.rightColor}
+                      
+                    />    
+                </div>
+            </div>
+            
+                <button 
+                    type="submit" 
+                    className="btn btn-primary float-right"
+                >
+                    Submit
+                </button>
+            </form>
+        </div>
+    
         )
     }
 }
